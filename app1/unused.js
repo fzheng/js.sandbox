574cba0bbbca905df9bf772511dfe53d149b0243
Feng Zheng <fzheng@users.noreply.github.com>
2016-05-31 22:32:49 -0400

    Update unused.js

--- app1/unused.js
+++ app1/unused.js
@@ -2,6 +2,6 @@ module.exports = function (app) {
   app.disable('x-powered-by');
 
   app.post('/not/imported', function (req, res) {
-    console.log(req.params.num);
+    res.send(req.params.num);
   });
 };
