cfcae51db68cc002d281af1138a0f8f49099455b
Feng Zheng <fzheng@codiscope.com>
2016-12-13 14:58:50 -0500

    random app

---
+++ app6/src/Scheduler.java
@@ -0,0 +1,96 @@
+import java.util.PriorityQueue;
+import java.util.concurrent.atomic.AtomicInteger;
+
+
+public class Scheduler {
+    private PriorityQueue<Task> tasks;
+    private final Thread taskRunnerThread;
+    private volatile boolean running;
+    private final AtomicInteger taskId;
+
+    public Scheduler() {
+        tasks = new PriorityQueue<>();
+        taskRunnerThread = new Thread(new TaskRunner());
+        running = false;
+        taskId = new AtomicInteger(0);
+        taskRunnerThread.start();
+    }
+
+    public void schedule(Task task, long delayMs) {
+        long timeToRun = System.currentTimeMillis() + delayMs;
+        task.setTimeToRun(timeToRun);
+        task.setId(taskId.incrementAndGet());
+        synchronized (this) {
+            tasks.offer(task);
+            this.notify();
+        }
+    }
+
+    public void stop() throws InterruptedException {
+        synchronized (this) {
+            running = false;
+            this.notify();
+        }
+        taskRunnerThread.join();
+    }
+
+    public static class Task implements Comparable<Task> {
+        private long timeToRun;
+        private int id;
+
+        public long getTimeToRun() {
+            return timeToRun;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setTimeToRun(long timeToRun) {
+            this.timeToRun = timeToRun;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public void run() {
+            System.out.println("Running task " + id);
+        }
+
+        public int compareTo(Task other) {
+            return (int) (timeToRun - other.getTimeToRun());
+        }
+    }
+
+    private class TaskRunner implements Runnable {
+        @Override
+        public void run() {
+            while (running) {
+                synchronized (Scheduler.this) {
+                    try {
+                        while (running && tasks.isEmpty()) {
+                            Scheduler.this.wait();
+                        }
+                        long now = System.currentTimeMillis();
+                        Task t = tasks.peek();
+                        if (t.getTimeToRun() < now) {
+                            tasks.poll();
+                            t.run();
+                        } else {
+                            Scheduler.this.wait(t.getTimeToRun() - now);
+                        }
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Scheduler scheduler = new Scheduler();
+        scheduler.schedule(new Task(), 100000);
+        scheduler.schedule(new Task(), 1000);
+    }
+}
