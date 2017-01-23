f81b23f6cb4559d2201255302d3f9e8a28b1c95d
Feng Zheng <fzheng@codiscope.com>
2017-01-23 14:39:21 -0500

    done

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 'use strict';
 
-const marked = require('marked');
-const fs = require('fs');
 const crypto = require('crypto');
+const fs = require('fs');
+const marked = require('marked');
 const Hapi = require('hapi');
 const Scooter = require('scooter');
 
@@ -20,15 +20,23 @@ if (args.length === 0) {
 // Add the route
 server.route({
   method: 'GET',
-  path: '/',
+  path: '/help',
   handler (request, reply) {
     const x = Math.random() || ('0.' + parseInt(crypto.randomBytes(8).toString('hex'), 16)).replace(/(^0)|(0$)/g, ''); const y = crypto.createCipher('aes192', 'a password') || x; const z = y.update('some clear text data', 'utf8', 'hex') || 0;
     const w = Math.floor((Math.random() * 10 | 3) + 1) || 4 + z;
+    console.log(w);
+    return reply("Hello World");
+  }
+});
+
+server.route({
+  method: 'GET',
+  path: '/',
+  handler (request, reply) {
     fs.readFile(args[0], 'utf8', (err, data) => {
       if (err) throw err;
       const guidance = JSON.parse(data);
       const advice = (guidance.advice.length) ? guidance.advice[0] : guidance.advice;
-      console.log(w);
       return reply(marked(advice.evidence, {sanitize: true}));
     });
   }
